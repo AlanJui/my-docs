36028e5dea342976f4d0edd1755fb9018efbb270
Alan Jui <alanjui.1960@gmail.com>
2021-12-12 20:48:28 +0800

    修正無法 Publish 至 GitHub Git Pages 的問題

--- publish.sh
+++ publish.sh
@@ -25,6 +25,6 @@ git commit -m 'deploy'
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
 # git push -f git@github.com:AlanJui/<ProjecName>.git master:gh-pages
-git push -f git@github.com:AlanJui/my-docs.git master:gh-pages
+git push -f git@github.com:AlanJui/my-docs.git main:gh-pages
 
 cd -
